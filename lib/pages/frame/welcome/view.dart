00745f12978c4211e78745bed5aa56623fc3580b
Elavarasan <elavarasa.003@gmail.com>
2023-08-18 19:02:38 +0530

    i init for some add functions

---
+++ lib/pages/frame/welcome/view.dart
@@ -0,0 +1,43 @@
+import 'package:chatty/common/values/values.dart';
+import 'package:chatty/pages/frame/welcome/controller.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class WelcomePage extends GetView<WelcomeController> {
+  const WelcomePage({super.key});
+
+  Widget _buildPageHeadTitle(String title) {
+    return Container(
+      margin: EdgeInsets.only(top: 350),
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style:  TextStyle(
+          color: AppColors.primaryElementText,
+          fontFamily: "Montserrat",
+          fontWeight:FontWeight.bold,
+          fontSize: 45.sp
+        ),
+        ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryElement,
+     
+      body: Container(
+          width: 360.w,
+      height: 780.h,
+        child: _buildPageHeadTitle(controller.title),
+
+      )
+    );
+  }
+}
+
+
